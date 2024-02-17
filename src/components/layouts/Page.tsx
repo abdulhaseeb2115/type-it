52c0a608a8216ec49f2c689001ff7371f67de78a
abdulhaseeb2115 <abdulhaseeb2115@gmail.com>
2024-02-17 05:25:56 +0500

    small updates

--- src/components/layouts/Page.tsx
+++ src/components/layouts/Page.tsx
@@ -1,15 +1,19 @@
-import React, { ReactNode } from "react";
+import React, { HTMLProps, ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 //----------------------------------------------
 
 type Props = {
 	children: ReactNode;
 	className?: string;
-};
+} & HTMLProps<HTMLElement>;
 
 //----------------------------------------------
-const Page = ({ children, className }: Props) => {
-	return <main className={twMerge("", className)}>{children}</main>;
+const Page = ({ children, className, ...props }: Props) => {
+	return (
+		<main className={twMerge("", className)} {...props}>
+			{children}
+		</main>
+	);
 };
 
 export default Page;
--- src/components/layouts/Section.tsx
+++ src/components/layouts/Section.tsx
@@ -1,16 +1,19 @@
-import React, { ReactNode } from "react";
+import React, { HTMLProps, ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 //----------------------------------------------
 
 type Props = {
 	children: ReactNode;
 	className?: string;
-};
+} & HTMLProps<HTMLElement>;
 
 //----------------------------------------------
-const Section = ({ children, className }: Props) => {
+const Section = ({ children, className, ...props }: Props) => {
 	return (
-		<section className={twMerge("p-6 md:p-10 lg:p-20 xl:p-24", className)}>
+		<section
+			className={twMerge("p-6 md:p-10 lg:p-20 xl:p-24", className)}
+			{...props}
+		>
 			{children}
 		</section>
 	);
