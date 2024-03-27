17e68bf69bd69d878f0aa74ccbd498726f9aabd4
abdulhaseeb2115 <abdulhaseeb2115@gmail.com>
2024-03-28 01:17:22 +0500

    updated the modal

--- src/components/layouts/Modal.tsx
+++ src/components/layouts/Modal.tsx
@@ -4,6 +4,8 @@ import { twMerge } from "tailwind-merge";
 import ChevronRightIcon from "../icons/ChevronRightIcon";
 // hooks
 import UseClickOutside from "@/hooks/UseClickOutside";
+// components
+import GlassmorphismCard from "../GlassmorphismCard";
 
 //----------------------------------------------
 
@@ -16,11 +18,12 @@ type Props = {
 
 const modalStyles = {
 	modal:
-		"absolute top-0 right-0 w-[350px] md:w-[500px] h-fit m-4 p-2 flex flex-col rounded-xl bg-dark-1 bg-opacity-20 backdrop-blur-lg border border-dark-1 border-opacity-40 shadow-2xl shadow-black z-[9999] duration-500 ease-in-out",
+		"absolute top-0 right-0 w-[350px] md:w-[500px] h-fit m-4 flex flex-col z-[9999] duration-500 ease-in-out",
 	closed: "translate-x-[200%]",
-	container: "relative h-full py-16 3xl:py-24",
+	container:
+		"relative h-full py-16 3xl:py-24 rounded-xl shadow-2xl shadow-black",
 	button:
-		"absolute top-0 right-0 p-1 hover:opacity-70 duration-200 ease-in-out",
+		"absolute top-0 right-1 p-1 hover:opacity-70 duration-200 ease-in-out",
 	buttonIcon: "h-8 aspect-square stroke-white",
 };
 
@@ -35,21 +38,21 @@ const Modal = ({ children, className, open, onClose }: Props) => {
 	});
 
 	return (
-		<aside
-			ref={(node) => clickOutsideRef(node)}
+		<div
 			className={twMerge(
 				modalStyles.modal,
 				!open && modalStyles.closed,
 				className
 			)}
+			ref={(node) => clickOutsideRef(node)}
 		>
-			<div className={modalStyles.container}>
+			<GlassmorphismCard className={modalStyles.container}>
 				<button className={modalStyles.button} onClick={closeModal}>
 					<ChevronRightIcon className={modalStyles.buttonIcon} />
 				</button>
 				{children}
-			</div>
-		</aside>
+			</GlassmorphismCard>
+		</div>
 	);
 };
 
